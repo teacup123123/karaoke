2a2bead7f8d054c6b4ec001840445528c12cdd68
tikai <teacup123123@gmail.com>
2019-03-14 20:39:41 +0200

    color code

--- docs/js/definePitchColor.js
+++ docs/js/definePitchColor.js
@@ -1,5 +1,19 @@
+var _pitch_dict = {};
+_pitch_dict[0] = ['magenta','violet'];//A 
+_pitch_dict[1] = ['mediumpurple','grey'];//A~B
+_pitch_dict[2] = ['darkmagenta','mediumpurple'];//B
+_pitch_dict[3] = ['crimson','red'];//C
+_pitch_dict[4] = ['darkred','grey'];//C~D
+_pitch_dict[5] = ['darkorange','orange'];//D
+_pitch_dict[6] = ['firebrick','grey'];//D~E
+_pitch_dict[7] = ['gold','yellow'];//E
+_pitch_dict[8] = ['darkgreen','green'];//F
+_pitch_dict[9] = ['darkseagreen','grey'];//F~G
+_pitch_dict[10] = ['teal','darkturquoise'];//G
+_pitch_dict[11] = ['thistle','grey'];//G~A
+
 function pitchColor(pitch)
 {
-	color = ["blue","red"]//in reality this should be a function of the pitch!!!
+	color = _pitch_dict[pitch%12]//in reality this should be a function of the pitch!!!
 	return color
 }
