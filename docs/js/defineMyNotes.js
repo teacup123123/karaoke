21b28cdf56ff3d6688bc6a05edaa333f8d2a174e
tikai <teacup123123@gmail.com>
2019-03-14 19:41:28 +0200

    test

--- docs/js/defineMyNotes.js
+++ docs/js/defineMyNotes.js
@@ -1,10 +1,14 @@
 var progress = 0
 var myNotes = [];//{timeStart : 0, timeEnd :10, pitch :4 ,lyric: "miao"}]; time in miliseconds
 
-function loadNotes(src)
+function loadNotes()
 {
 	progress = 0;
 	myNotes = [];
+	
+	var list = document.getElementById("list");
+	var info = document.getElementById("info");
+	info.innerHTML='loaded!!'
 	
 	myNotes.push(new myNote(0, 1000,1,lyric='1'))
 	myNotes.push(new myNote(1000, 2000,2,lyric='2'))
@@ -16,7 +20,20 @@ function refreshList() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("list").innerHTML = this.responseText;
+			var list = document.getElementById("list");
+			
+			while (list.length > 0) {
+				list.remove(0);
+			}
+			
+			var jsonList = JSON.parse(this.responseText);
+			for (index = 0; index < jsonList.length; ++index) {
+				var song = jsonList[index];
+				var opti = document.createElement('option');
+				opti.text = song.title;
+				list.add(opti)
+			}
+			
 		}
 	};
 	xhttp.open("GET", "json/list.json", true);
