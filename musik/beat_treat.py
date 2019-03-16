893cf2c1de41bf4d235213b83594834ed5cc54f1
tikai <teacup123123@gmail.com>
2019-03-16 12:45:07 +0200

    treat musicxml files

--- musik/beat_treat.py
+++ musik/beat_treat.py
@@ -1,24 +1,126 @@
 from scipy.io import wavfile as wav
 import numpy as np
+import matplotlib.pyplot as pl
+import xml.etree.ElementTree as ET
+import musik.common as cm
+from scipy import interpolate
+import json
 
-## load data
-rate, data = wav.read('exports/基準 in A.wav')
 
-left = data[:, 0]
-right = data[:, 1]
+def adjustPartTempo(part, tempos):
+    for measure in part:
+        measure: ET.Element = measure
 
-## detection
-A4 = 440
-# lowest = B3 (-10)
-# highest = D6 (24+5)
+        direction: ET.Element = measure.find('direction')
+        if direction is None:
+            direction = ET.SubElement(measure, 'direction')
 
-probe = np.arange(-10, 30, 0.25)
-frequencies = 2 ** (probe / 12) * A4
-dfs = frequencies * (2 ** (1 / 48) - 1)
-freqsNormed=dfs/rate
-samplenums = np.round(2/freqsNormed) # window should be - this number ~ 0
+        sound: ET.Element = direction.find('sound')
+        if sound is None:
+            sound = ET.SubElement(direction, 'sound')
 
-cumsums=np.zeros((np.size(probe),),dtype=int)
+        sound.attrib['tempo'] = "100"
 
-window = 0.25  # s
-windowSamples = int(window * rate)
+if __name__ == '__main__':
+
+    ## load data
+    etree = cm.loadxml('../score/0316.musicxml')
+    root: ET.Element = etree.getroot()
+    beat_lines, vocal_lines, part_list = cm.findParts(root)
+    rate, data = wav.read('../treated/corrected0314/beat.wav')
+    batchname = 'portOjisan'
+
+    # left = data[:, 0]
+    # right = data[:, 1]
+    # data = left + right
+    data = abs(data)
+    # assert all(x>=0 for x in data)
+
+    window = 0.005  # 5ms = 200Hz
+    window *= rate  # now in sample numbers
+    window = int(window)
+    print('window = int(window) = {}'.format(window))
+
+    # pl.plot(data[window:4*window])
+    # pl.show()
+
+    nbwindow = len(data) // window
+    print('nbwindow = len(data)={}//window'.format(nbwindow))
+    data: np.ndarray = data[:nbwindow * window]
+    data = data.reshape((nbwindow, window))
+    downsample = data.sum(axis=1)
+    ts = np.arange(len(downsample)) * window / rate * 1000  # ms
+
+    # upwards = np.sign(np.diff(downsample))==1
+    thresh = np.max(downsample) * 0.4
+    bigger = downsample >= thresh
+    smaller = downsample < thresh
+    bigger = np.roll(bigger, 1)
+    crossing = np.logical_and(bigger, smaller)
+    beats = np.logical_and(downsample > thresh, crossing)
+
+    # pl.plot(ts,crossing)
+    # pl.show()
+
+    _ = np.nonzero(crossing)
+    ts = ts[_]
+
+    _ = zip([b[0] for b in beat_lines], ts)
+    beat, beatInMs = zip(*_)
+
+    f2 = interpolate.interp1d(beat, beatInMs, kind='cubic')
+
+
+    def beat2time(beatnum):
+        if beatnum >= beat[-1]:
+            endingSlope = (beatInMs[-1] - beatInMs[-2]) / (beat[-1] - beat[-2])
+            return (beatnum - beat[-1]) * endingSlope + beatInMs[-1]
+        else:
+            _ = f2(beatnum)
+            if type(_) == np.ndarray and np.size(_) == 1:
+                _ = float(_)
+            return _
+
+
+    # referencePitch = cm.pitchFromXmlNotation('0 C ')
+    for vli, l in enumerate(vocal_lines):
+        notesForJson = []
+        for start, end, pitch, lyr in l:
+            startms, endms = beat2time(start), beat2time(end)
+            note = {'start': int(startms), 'end': int(endms), 'pitch': int(pitch), 'lyric': lyr}
+            notesForJson.append(note)
+        minPitchi = int(np.argmin([entry['pitch'] for entry in notesForJson]))
+        maxPitchi = int(np.argmax([entry['pitch'] for entry in notesForJson]))
+        maxPitch = notesForJson[maxPitchi]['pitch']
+        minPitch = notesForJson[minPitchi]['pitch']
+        ref = 12 * np.floor(minPitch / 12.)
+        content = {'notes': notesForJson, 'referencePitch': int(ref),
+                   'range': maxPitch - ref + 4, 'vocalmin': minPitch, 'vocalmax': maxPitch}
+        content = json.dumps(content)
+        with open('../docs/json/{}{}.json'.format(batchname, vli), 'w', encoding='utf8') as f:
+            f.write(content)
+
+
+    adjustPartTempo(part_list[0], [])
+    etree.write('../docs/xml/{}.xml'.format(batchname))
+
+    part_listnew = [ET.fromstring(ET.tostring(x)) for x in part_list]
+    part_listDeclnew = [ET.fromstring(ET.tostring(x)) for x in root.find('part-list')]
+
+    for parti, (part, partDec) in enumerate(zip(part_listnew, part_listDeclnew)):
+        part: ET.Element = part
+
+        pldec = root.find('part-list')
+        for x in pldec.findall('score-part'):
+            pldec.remove(x)
+
+        for p in root.findall('part'):
+            if p.tag=='part':
+                root.remove(p)
+
+        adjustPartTempo(part, [])
+
+
+        pldec.insert(0, partDec)
+        root.insert(5, part)
+        etree.write('../docs/xml/{}{}.xml'.format(batchname, parti))
--- musik/common.py
+++ musik/common.py
@@ -1,5 +1,6 @@
 import itertools
 import xml.etree.ElementTree as ET
+import numpy as np
 
 simple_map = {1: 0,
               2: 2,
@@ -50,6 +51,156 @@ def loadLyr(file):
         return pronounciation
 
 
+def fifthdict(fifthnum: int):
+    pitchModulos = np.array([0,2,4,5,7,9,11],dtype=int)
+    pitchModulos = np.mod(pitchModulos + 7 * fifthnum,12)
+    pitchModulos = np.sort(pitchModulos)
+    if np.sign(fifthnum)<0 and pitchModulos[0]==1:
+        pitchModulos = np.roll(pitchModulos,1)
+        pitchModulos[0]-=12
+
+    items = zip('CDEFGAB',pitchModulos)
+    return dict(items)
+
+
+def pitchFromXmlNotation(pitchstr: str):
+    '''
+    returns 0 for C2
+    half tone = 1
+    -1 used as drumbeat, whose lyrics change keys
+
+    :param pitchstr:
+    :return:
+    '''
+
+    strlist = pitchstr.split()
+    if strlist[0] == 'drum':
+        return -1
+
+    fifth = fifthdict(int(strlist[0]))
+    note = fifth[strlist[1]]+ int(strlist[2])*12
+    alteration = int(strlist[3]) if len(strlist)==4 else 0
+
+    return note + alteration
+
+
+def findParts(root: ET.Element):
+    part_liste: ET.Element = root.find('part-list')
+    part_list = part_liste.findall('score-part')
+    found_beat_part_num = None
+    for score_part in part_list:
+        score_part: ET.Element
+        instrument_name: ET.Element = score_part.find('part-name')
+        if instrument_name.text == 'Finger Snap':
+            print('found Finger Snap, using as beat')
+            found_beat_part_num = score_part.attrib['id']
+
+            #delete this part from the xml tree since finger snap is not good
+            part_liste.remove(score_part)
+
+    vocal_lines = []
+    beat_lines = []
+    part_list = root.findall('part')
+    for part in part_list:
+        part: ET.Element  # containing many measures
+        vocal_lines_in_part = dict()
+
+        meas_list = part.findall('measure')
+        globaltime = 0
+        keySig = 'x'
+        for measi, meas in enumerate(meas_list):
+            meas: ET.Element
+            for _ in meas.findall('direction'):
+                meas.remove(_)
+            attr: ET.Element = meas.find('attributes')
+            if attr is not None:
+                key: ET.Element = attr.find('key')
+                if key is not None:
+                    fifth: ET.Element = key.find('fifths')
+                    keySig = fifth.text
+
+            localTime = 0
+
+            for elt in meas:
+                elt: ET.Element
+                if elt.tag in ['note', 'backup']:
+                    if elt.tag == 'backup':
+                        localTime = 0
+                    else:
+                        assert elt.tag == 'note'
+                        voice: ET.Element = elt.find('voice')
+                        vocal_lines_in_part[voice.text] = vocal_lines_in_part.get(voice.text, [])
+                        rest = elt.find(
+                            'rest')
+                        dur = int(elt.find('duration').text)
+                        notations: ET.Element = elt.find('notations')
+                        tied: ET.Element = notations.find('tied') if notations is not None else None
+                        if rest is not None:
+                            localTime += dur
+                        else:
+                            start = globaltime + localTime
+                            lyr: ET.Element = elt.find('lyric')
+                            lyr: ET.Element = lyr.find('text') if lyr is not None else None
+                            pitch: ET.Element = elt.find('pitch')
+                            if pitch is not None:
+                                step: ET.Element = pitch.find('step')
+                                octave: ET.Element = pitch.find('octave')
+                                alter: ET.Element = pitch.find('alter')
+                                altertext = '' if alter is None else alter.text
+                                pitch = ' '.join([keySig, step.text, octave.text, altertext])  # TODO
+                            else:
+                                pitch = 'drum'
+
+                            if lyr is not None:
+                                lyr = lyr.text
+                            else:
+                                lyr = ''
+                            note = (start,
+                                    start + dur,
+                                    pitchFromXmlNotation(pitch),
+                                    lyr,
+                                    True if tied is not None and tied.attrib['type'] == 'start' else False)
+                            vocal_lines_in_part[voice.text].append(note)
+                            localTime += dur
+
+            globaltime += localTime
+            # print('meas {} of part {}'.format(measi,part.attrib['id']))
+
+            # print(elt.tag)
+            # pass
+            # i=meas.iter()
+
+        for key in vocal_lines_in_part.keys():
+            # link up
+            def iterator():
+                start0, end0, lyr0 = None, None, ''
+                for start, end, pitch, lyr, accumulate in vocal_lines_in_part[key]:
+                    if start0 == None:  # first
+                        start0 = start
+                    end0 = end
+                    lyr0 += lyr
+
+                    if not accumulate:
+                        # save
+                        yield start0, end0, pitch, lyr0
+                        # reset
+                        start0, end0, lyr0 = None, None, ''
+
+            vocal_lines_in_part[key] = [x for x in iterator()]
+
+            print('{}: {}'.format(key, vocal_lines_in_part[key]))
+        vocal_lines_in_part = [v for k, v in vocal_lines_in_part.items()]
+        if part.attrib['id'] == found_beat_part_num:
+            beat_lines = vocal_lines_in_part[0]
+
+            #delete this part from the xml tree since finger snap is not good
+            root.remove(part)
+        else:
+            vocal_lines.extend(vocal_lines_in_part)
+    print('plucked all info from sheet')
+    return beat_lines, vocal_lines,part_list
+
+
 if __name__ == '__main__':
     tree = loadxml('score/db.musicxml')
     lyrfile = 'treated/pinyin_bass'
@@ -57,7 +208,7 @@ if __name__ == '__main__':
 
     root = tree.getroot()
 
-    part:ET.Element = root.find('part')
+    part: ET.Element = root.find('part')
 
     measures = part.findall('measure')
     notes = [[note for note in m.findall('note')] for m in measures]
@@ -88,10 +239,9 @@ if __name__ == '__main__':
             syllabic.text = 'single'
             count += 1
 
-    with open('template.xml',encoding='utf8') as f:
-        with open('output.xml','w',encoding='utf8',) as fo:
-            everything=f.read(-1)
-            partstr=ET.tostring(part,encoding="unicode")
+    with open('template.xml', encoding='utf8') as f:
+        with open('output.xml', 'w', encoding='utf8', ) as fo:
+            everything = f.read(-1)
+            partstr = ET.tostring(part, encoding="unicode")
             fo.write(everything.format(part1=partstr))
             pass
-
